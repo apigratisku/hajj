9a04291abfdc9c0cbf160d9b33f34eab7961f186
Adiarizki-Laptop <adhit.bisnis@gmail.com>
2025-08-13 16:16:34 +0800

    Update penambahan logo web

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($title) ? $title . ' - ' : '' ?>Sistem Pendataan Kunjungan Peserta Haji dan Umrah</title>
+    <link rel="icon" href="<?= base_url('assets/img/hajj_logo.png') ?>" type="image/x-icon">
     
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <!-- Custom CSS -->
-    <link rel="icon" href="<?= base_url('assets/img/hajj_logo.png') ?>" type="image/x-icon">
     <style>
         :root {
             --primary-color: #8B4513;
